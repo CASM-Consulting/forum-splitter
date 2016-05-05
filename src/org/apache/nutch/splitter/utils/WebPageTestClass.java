5a34c7ba6c8d7e856e7af4ea62a55290ff092a58
jaquefoo <jp242@sussex.ac.uk>
2016-05-05 12:11:48 +0100

    updated scrapers

--- src/org/apache/nutch/splitter/utils/WebPageTestClass.java
+++ src/org/apache/nutch/splitter/utils/WebPageTestClass.java
@@ -15,22 +15,23 @@ import org.jsoup.nodes.Element;
 
 public class WebPageTestClass {
 	
-	private static final String BODY_NAME = "post";
+	private static final String BODY_NAME = "alt1";
 	private static final String CONTENT = "cPost_contentWrap";
 	
 	
 	public static void run(String page) {
 		try {
 			Document doc = Jsoup.connect(page).userAgent("Mozilla").get();
-			for(Element elem : doc.getElementsByClass("cPost")) {
-				System.out.println(elem.getElementsByClass(CONTENT).text());
+			for(Element elem : doc.select("tr")) {
+				System.out.println(elem.text());
+//				System.out.println(elem.getElementsByClass(CONTENT).text());
 	//				System.out.println(elem.getElementsByClass("username").first().text());
 	//				System.out.println(elem.getElementsByClass("author").text());
 			}
 //			String[] s = doc.getElementsByClass("author").first().text().split("\\s");
 //			String dateS = s[4] + " " + s[5].replace(",","") + " " + s[6];
 ////			String toks = doc.getElementsByClass("post-username").first().getElementsByClass("avatar-hover").first().text();
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd yyyy");
+//			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd yyyy");
 //			LocalDate date = LocalDate.parse(dateS,dtf);
 ////			LocalDate date = LocalDate.from(ta);
 ////			LocalDate date = LocalDate.parse("Tue Apr 2016",dtf);
@@ -47,7 +48,7 @@ public class WebPageTestClass {
 		
 //		System.out.println(LocalDate.now().atStartOfDay().toString());
 		
-		String page = "http://www.depressionforums.org/forums/topic/117175-whole-days-of-sluggishtired/";
+		String page = "http://www.healthboards.com/boards/mens-health/746493-relief-red-scrotum-syndrome.html";
 		WebPageTestClass.run(page);
 		
 	}
